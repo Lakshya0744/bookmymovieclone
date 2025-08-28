58da3e6d08a56ae80d4a0ec53da77546f71502e4
Lakshya0744 <55252889+Lakshya0744@users.noreply.github.com>
2025-08-28 16:24:32 +0530

    Managing CORS issues

--- bookmymovie-frontend/public/config.js
+++ bookmymovie-frontend/public/config.js
@@ -1,10 +1,3 @@
-const isLocal =
-  window.location.hostname === "localhost" ||
-  window.location.hostname.startsWith("192.168.") ||
-  window.location.hostname === "127.0.0.1";
-
 window.RUNTIME_CONFIG = {
-  API_BASE: isLocal
-    ? "http://192.168.0.152:8081"         // local backend
-    : "https://bookmymovie-backend-9bg7.onrender.com/" // render backend
+  API_BASE: "https://bookmymovie-backend-9bg7.onrender.com"
 };
