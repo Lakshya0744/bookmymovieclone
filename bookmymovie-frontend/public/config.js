8469534ec3ef5ae87cd2541a53e045069ebc68c6
Lakshya0744 <55252889+Lakshya0744@users.noreply.github.com>
2025-08-28 15:55:30 +0530

    setting up the database

--- bookmymovie-frontend/public/config.js
+++ bookmymovie-frontend/public/config.js
@@ -1,3 +1,10 @@
+const isLocal =
+  window.location.hostname === "localhost" ||
+  window.location.hostname.startsWith("192.168.") ||
+  window.location.hostname === "127.0.0.1";
+
 window.RUNTIME_CONFIG = {
-  API_BASE: "http://192.168.0.152:8081"
+  API_BASE: isLocal
+    ? "http://192.168.0.152:8081"         // local backend
+    : "https://bookmymovie-backend-9bg7.onrender.com/" // render backend
 };
